916ed0853278100a41985d8edb543410d3c2562c
Santiago Rincon <santiago.rincon@inlabdigital.com>
2022-12-17 10:55:44 +0100

    New: upgrade version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='autoexplain',
-    version='1.0.3',
+    version='1.0.4',
     description='Use OpenAI capabilities to smooth your document generation',
     url='https://github.com/rincon-santi/autoexplain',
     author='Santiago Rincon Martinez',
